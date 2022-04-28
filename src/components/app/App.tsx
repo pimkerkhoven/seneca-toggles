0583521bd6a0bab62631e0afb14f18b2394bd0b0
Pim Kerkhoven <pim.kerkhoven@pm.me>
2022-04-28 23:00:31 +0200

    Comment code; update names; add todos

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 
 // TODO: refactor types to own file
 // TODO: add assumption to Readme
+// TODO: also have assumptions in comments surrounding code
 // Assume answer is included in options
 interface QuestionPart {
   options: string[]
@@ -13,10 +14,13 @@ interface Question {
   parts: QuestionPart[]
 }
 
+// TODO: add component level (multi-line) comment indicating purpose/intent
 function App() {
   const question: Question = {
     // TODO: Where to format question? Assume is in correct format (capitalization?)
     //  Same for options
+    // TODO: Assume options are all unique within part
+    // TODO: State assumption of question format.
     title: "An animal cell contains",
     parts: [
       {
@@ -34,15 +38,19 @@ function App() {
     ]
   }
 
-  // TODO: not necessary, can be determined based on locked
-  const [locked, setLocked] = useState<boolean>(false)
+  // Indicates whether the question is answered correctly (i.e. if all parts are answered correctly)
+  // TODO: consistent naming of boolean variables is.../are.../has...
+  const [isSolved, setIsSolved] = useState<boolean>(false)
 
+  // TODO: maybe this works a bit nicer if answers is an object?  with a key for each part? How
+  //  will typescript handle this?
+  //  -> https://stackoverflow.com/questions/41045924/how-to-represent-a-variable-key-name-in-typescript-interface
   // TODO: make sure never starts in answered formation
   const [answers, setAnswers] = useState<string[]>(question.parts.map(part => part.options[0]))
 
   // TODO: include event.preventDefault
-  const handleClickAnswer = (partIndex: number, option: string) => {
-    if (locked) {
+  const handleToggleAnswer = (partIndex: number, option: string) => {
+    if (isSolved) {
       return
     }
 
@@ -51,15 +59,20 @@ function App() {
   }
 
   useEffect(() => {
-    const answeredCorrectly = answers.reduce((accumulator, currentAnswer, index) => {
+    // Check for each question if the current answer is the actual answer for the question
+    // Combine this result with the result of the previous questions
+    // TODO: maybe there is a dedicated array function for this?
+    const areAllAnswersCorrect = answers.reduce((accumulator, currentAnswer, index) => {
       return accumulator && question.parts[index].answer === currentAnswer
     }, true)
 
-    if (answeredCorrectly) {
-      setLocked(true)
+    if (areAllAnswersCorrect) {
+      setIsSolved(true)
     }
   }, [answers, question.parts])
 
+  // TODO: change cursor to pointer when hovering (un)selected options
+  // TODO: search if there are accessibility/better options than just using divs
   return (
     <div className="App">
       <h1>{question.title}</h1>
@@ -68,13 +81,13 @@ function App() {
             {part.options.map(option => {
               const style = option === answers[partIndex] ? { fontWeight : "bold" } : {}
 
-              return <div key={option} style={style} onClick={() => handleClickAnswer(partIndex, option)}>
+              return <div key={option} style={style} onClick={() => handleToggleAnswer(partIndex, option)}>
                 {option}
               </div>
             })}
           </div>
       ))}
-      <h1>{locked ? "The answer is correct!" : "The answer is incorrect"}</h1>
+      <h1>{isSolved ? "The answer is correct!" : "The answer is incorrect"}</h1>
     </div>
   );
 }
