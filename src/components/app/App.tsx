326bee82bc72f050f9c0ae12b52b20fcfdd6535b
Pim Kerkhoven <pim.kerkhoven@pm.me>
2022-05-02 21:38:07 +0200

    Change between multiple questions

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Question } from "../../types/Question";
 import TogglesQuestion from "../toggles-question/TogglesQuestion";
 
 // TODO: add component level (multi-line) comment indicating purpose/intent
 function App() {
-  const question: Question = {
+  const questions: Question[] = [{
     // TODO: Where to format question? Assume is in correct format (capitalization?)
     //  Same for options
     // TODO: Assume options are all unique within part
@@ -24,12 +24,37 @@ function App() {
         answer: "Impermeable membrane"
       }
     ]
+  }, {
+    title: "Things I can see right now:",
+    parts: [
+      {
+        options: ["Cat", "Dog"],
+        answer: "Cat"
+      },
+      {
+        options: ["Ecology book", "Geology book"],
+        answer: "Ecology book"
+      },
+      {
+        options: ["Apple", "Banana", "Water melon"],
+        answer: "Apple"
+      }
+    ]
+  }]
+
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState<number>(0)
+
+  function nextQuestion(event: React.MouseEvent) {
+    event.preventDefault()
+
+    setActiveQuestionIndex(prevState => (prevState + 1) % questions.length)
   }
 
   // TODO: change cursor to pointer when hovering (un)selected options
   // TODO: search if there are accessibility/better options than just using divs
   return <div style={{maxWidth: 900, margin: "0 auto"}}>
-    <TogglesQuestion question={question} />
+    <button onClick={nextQuestion}>Next Question</button>
+    <TogglesQuestion question={questions[activeQuestionIndex]} />
   </div>
 }
 
